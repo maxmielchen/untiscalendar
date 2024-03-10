c6e38eed2f30399a14f6059ad4e36e38d6c6db58
maxmielchen <max@mielchen.email>
2024-03-10 21:06:30 +0100

    Add Config class for managing application configuration

---
+++ app/src/main/java/io/mielchen.max/untiscalendar/app/Config.kt
@@ -0,0 +1,21 @@
+package io.mielchen.max.untiscalendar.app
+
+object Config
+{
+    var timezone : String = System.getenv("TIMEZONE")
+    var defaultRoomAlias : String = System.getenv("ROOM")
+    var defaultTeacherAlias : String = System.getenv("TEACHER")
+    var defaultSummary : String = System.getenv("SUMMARY")
+    var username : String = System.getenv("USERNAME")
+    var password : String = System.getenv("PASSWORD")
+    var server : String = System.getenv("SERVER")
+    var school : String = System.getenv("SCHOOL")
+    var ssl : Boolean = System.getenv("SSL") == "true"
+
+    fun getURL() : String
+    {
+        if (ssl) return "https://${server}" else return "http://${server}"
+    }
+
+    var token : String = System.getenv("TOKEN")
+}
