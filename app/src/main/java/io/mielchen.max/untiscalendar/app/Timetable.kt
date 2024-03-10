d9fbe50d58d5648574d06d25960dc1f11515ac54
maxmielchen <max@mielchen.email>
2024-03-10 21:06:38 +0100

    Add function to retrieve user-specific timetable

---
+++ app/src/main/java/io/mielchen.max/untiscalendar/app/Timetable.kt
@@ -0,0 +1,12 @@
+package io.mielchen.max.untiscalendar.app
+
+import org.bytedream.untis4j.Session
+import org.bytedream.untis4j.responseObjects.Timetable
+
+fun getUserSpecificTable() : Timetable
+{
+    val session = Session.login(Config.username, Config.password, Config.getURL(), Config.school)
+    val timetable = session.getTimetableFromPersonId(session.currentSchoolYear.startDate, session.currentSchoolYear.endDate, session.infos.personId)
+    session.logout()
+    return timetable
+}
