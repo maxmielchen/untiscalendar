77f78d7185704fbb024e06610f477a7ccbf02b10
maxmielchen <max@mielchen.email>
2024-03-10 21:06:25 +0100

    Add App.kt file with routing for iCal endpoint

---
+++ app/src/main/java/io/mielchen.max/untiscalendar/app/App.kt
@@ -0,0 +1,32 @@
+package io.mielchen.max.untiscalendar.app
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.slf4j.event.*
+
+fun main()
+{
+    embeddedServer(Netty, port = 8080, module = Application::module).start(true)
+}
+
+fun Application.module()
+{
+    install(CallLogging) {
+        level = Level.INFO
+    }
+    routing {
+        get("/ical") {
+            if (!call.request.queryParameters["token"].equals(Config.token)) return@get call.respond(
+                HttpStatusCode.Unauthorized,
+                "Wrong access token!"
+            )
+            call.response.header("Content-Type", "text/calendar")
+            return@get call.respond(HttpStatusCode.OK, getUserSpecificTable().getCalender().toString())
+        }
+    }
+}
